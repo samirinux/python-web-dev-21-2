2d6fac792324b1ac02d2043f6e19ae716df3ed2f
Samir El Mustafa au000mft <samir.elmustafa@wsa.com>
2022-04-09 02:12:02 +0200

    Implements blog and organiser models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,23 @@
+import imp
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+from organiser.models import Startup, Tag
+
+class Post(models.Model):
+    
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(max_length=63)
+    text = models.TextField()
+    pud_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
+
+    class Meta:
+        get_latest_by = "pub_date"
+        ordering = ["-pub_date", "title"]
+        verbose_name = "blog post"
+    
+    def __str__(self) -> str:
+        date_string = self.pud_date.strftime("%Y-%m-%d")
+        return f"{self.title} on {date_string}"
